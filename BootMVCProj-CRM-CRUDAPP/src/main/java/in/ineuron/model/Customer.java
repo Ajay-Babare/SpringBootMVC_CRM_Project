2bb95f4a5b1b2e98b5efa439f5832cc97277c6d6
Ajay-Babare <98898406+Ajay-Babare@users.noreply.github.com>
2023-07-27 17:21:08 +0530

    Add files via upload
    CRM Project

---
+++ BootMVCProj-CRM-CRUDAPP/src/main/java/in/ineuron/model/Customer.java
@@ -0,0 +1,47 @@
+package in.ineuron.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="customer")
+@Data
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
